8aeedfc61af319f569913ababbc8e55e2284151b
Rishi Sharma <39599919+kampaitees@users.noreply.github.com>
2019-02-24 22:58:37 +0530

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
-import scipy
 from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
+import scipy
 
 # Loading the data (cat/non-cat)
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
